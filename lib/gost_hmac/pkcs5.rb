5849ad048d3e699699584a4e40a8b5e318d09208
vblazhnov <vblazhnov@yandex.ru>
2019-04-16 16:39:27 +0300

    PKCS5 testing

--- lib/gost_hmac/pkcs5.rb
+++ lib/gost_hmac/pkcs5.rb
@@ -18,24 +18,39 @@ module GostHmac
         else
           outLen = keyLen
         end
-        itmp = [((count>>24)&0xff).chr,
-          ((count>>16)&0xff).chr,
-          ((count>>8)&0xff).chr
-          (count&0xff).chr]
+        itmp = [(count>>24)&0xff, (count>>16)&0xff, (count>>8)&0xff, count&0xff].pack('C*')
         buf = hmac_ctx.reset.update(salt).update(itmp).final
-        
+        puts '-'*40
+        printf("count = %d\n", count)
+        puts '-'*40
+        puts 'buf:'
+        printBytes(buf)
+        keyBuf[resPos...resPos+outLen] = buf[0...outLen]
+        puts 'keyBuf:'
+        printBytes(buf)        
         (1...iter_count).each do |i|
           buf = hmac_ctx.reset.update(buf).final
           (0...outLen).each do |j|
              keyBuf[resPos+j] = (keyBuf[resPos+j].ord ^ buf[j].ord).chr
           end        
         end
-        
+        puts 'Iterated keyBuf:'
+        printBytes(keyBuf)                
         keyLen -= outLen
         count += 1
         resPos += outLen
       end
+      puts '='*40
       key = keyBuf[0...keyLen].dup
+      puts 'key:'
+      printBytes(key)                
+      puts '='*40
+      key
     end
+    
+    def self.printBytes(bytes, line_size = 16)
+      bytes.unpack('H*')[0].scan(/.{1,#{line_size}}/).each{|s| puts(s)}
+    end
+
   end
 end
